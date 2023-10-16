4f9c934a0028b03fc200fffa01b53835284b1165
Iain <iain.mccall.95@gmail.com>
2023-10-16 19:31:37 +0100

    added header icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { categories } from "../backend";
 import { NavLink, Link } from "react-router-dom";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import { RiComputerLine } from "react-icons/ri";
 import Hamburger from "hamburger-react";
 
 const NavBar = () => {
@@ -20,7 +21,7 @@ const NavBar = () => {
         to={category.name}
         className={({ isActive }) =>
           isActive
-            ? `bg-indigo-300 ${navLinkStyle}`
+            ? `bg-gradient-to-tr from-orange-400 to-yellow-300 ${navLinkStyle}`
             : `bg-indigo-200 ${navLinkStyle}`
         }
       >
@@ -31,9 +32,13 @@ const NavBar = () => {
 
   return (
     <>
-      <div className="p-4 px-20 bg-slate-900 sticky top-0 flex items-center justify-between z-10">
-        <Link to="/">
-          <h1 className="text-4xl underline text-indigo-50">
+      <div className="p-4 px-20 bg-slate-900 sticky top-0 flex items-center justify-center  z-10">
+        <RiComputerLine
+          size={40}
+          className="fill-purple-500 mr-3 md:mr-6 md:flex-shrink-0"
+        />
+        <Link to="/" className="flex flex-wrap ">
+          <h1 className="md:text-4xl text-1xl text-transparent bg-clip-text bg-gradient-to-b from-purple-300 to-purple-500 mr-14">
             Web Development Resources
           </h1>
         </Link>
@@ -41,7 +46,9 @@ const NavBar = () => {
         {/* desktop buttons */}
         <ul className="hidden md:flex md:flex-wrap md:items-center">
           {navLinks}
-          <Link to="/about"><AiOutlineInfoCircle className="text-indigo-50 ml-3" size={35}/></Link>
+          <Link to="/about">
+            <AiOutlineInfoCircle className="text-indigo-50 ml-3" size={35} />
+          </Link>
         </ul>
 
         {/* mobile buttons */}
@@ -58,7 +65,6 @@ const NavBar = () => {
         }
       >
         {navLinks}
-        
       </ul>
     </>
   );
