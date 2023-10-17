e787bad4d74867a60b376f172b466c5bf563e9fc
Iain <iain.mccall.95@gmail.com>
2023-10-17 13:57:02 +0100

    added logos/links to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-        <p>
-            A collection of useful web development resources.
-        </p>
-    </div>
-  )
-}
 
-export default About
+  const imageStyle = "h-full w-fit translate-y-1 hover:drop-shadow-2xl hover:translate-y-0"
+
+  return (
+    <div className="flex justify-center text-center text-white text-2xl m-10">
+      <div>
+        <div className="flex h-60 w-full items-center justify-center mb-10">
+          <img
+            className={imageStyle}
+            src="https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png"
+          />
+          <img
+            className={imageStyle}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/512px-Tailwind_CSS_Logo.svg.png?20230715030042"
+          />
+          <img
+            className={imageStyle}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
+          />
+        </div>
+        <h2>
+          This site is a collection of useful web development resources created
+          with{" "}
+          <a
+            className="underline hover:text-yellow-300"
+            href="https://supabase.com/"
+          >
+            Supabase
+          </a>
+          ,{" "}
+          <a
+            className="underline hover:text-yellow-300"
+            href="https://tailwindcss.com/"
+          >
+            TailwindCSS
+          </a>{" "}
+          and{" "}
+          <a
+            className="underline hover:text-yellow-300"
+            href="https://react.dev/"
+          >
+            React
+          </a>
+          .
+        </h2>
+        <br></br>
+        <h2>
+          Check out the GitHub repo{" "}
+          <a
+            className="underline hover:text-yellow-300"
+            href="https://github.com/UhOhSpiders/web-dev-resources"
+          >
+            here
+          </a>
+          .
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,9 +12,10 @@ const NavBar = () => {
     setNav(!nav);
   };
 
+  const navLinkStyle =
+    "rounded-md text-text-base block mx-3 px-6 py-1 my-1  hover:bg-indigo-50 md:inline-block";
+
   const navLinks = categories.map((category, index) => {
-    let navLinkStyle =
-      "rounded-md text-text-base block mx-3 px-6 py-1 my-1  hover:bg-indigo-50 md:inline-block";
     return (
       <NavLink
         key={index}
@@ -30,15 +31,28 @@ const NavBar = () => {
     );
   });
 
+  navLinks.push(
+    <NavLink
+      to="/about"
+      className={({ isActive }) =>
+        isActive
+          ? `bg-gradient-to-tr from-orange-400 to-yellow-300 ${navLinkStyle}`
+          : `bg-indigo-200 ${navLinkStyle}`
+      }
+    >
+      <p>About</p>
+    </NavLink>
+  );
+
   return (
     <>
-      <div className="p-4 px-20 bg-slate-900 sticky top-0 flex items-center justify-center  z-10">
+      <div className="p-4 md:px-20 bg-slate-900 sticky top-0 flex justify-between items-center md:justify-center  z-10">
         <RiComputerLine
           size={40}
           className="fill-purple-500 mr-3 md:mr-6 md:flex-shrink-0"
         />
         <Link to="/" className="flex flex-wrap ">
-          <h1 className="md:text-4xl text-1xl text-transparent bg-clip-text bg-gradient-to-b from-purple-300 to-purple-500 mr-14">
+          <h1 className="md:text-4xl text-2xl text-transparent bg-clip-text bg-gradient-to-b from-purple-300 to-purple-500 mr-14">
             Web Development Resources
           </h1>
         </Link>
@@ -46,9 +60,6 @@ const NavBar = () => {
         {/* desktop buttons */}
         <ul className="hidden md:flex md:flex-wrap md:items-center">
           {navLinks}
-          <Link to="/about">
-            <AiOutlineInfoCircle className="text-indigo-50 ml-3" size={35} />
-          </Link>
         </ul>
 
         {/* mobile buttons */}
